8559cb1a59d2ba13b78e30963623deabf1c6904c
santicano <canosantiago404@gmail.com>
2024-02-22 16:38:41 -0300

    añadido billboard component

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,22 @@
-const HomePage = () => {
-  return <div>Hello store</div>;
+import getBillboard from "@/actions/get-billboards";
+
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("938834aa-9e97-4653-ac9b-19abb305df0b");
+
+  return (
+    <div>
+      <Container>
+        <div className="space-y-10 pb-10">
+          <Billboard data={billboard} />
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default HomePage;
