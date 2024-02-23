793c48a8e01d666e18841930ce4ed60e53961020
santicano <canosantiago404@gmail.com>
2024-02-23 14:55:55 -0300

    fixed typo

--- actions/get-brand.tsx
+++ actions/get-brand.tsx
@@ -1,6 +1,6 @@
 import { Brand } from "@/types";
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/brand`;
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/brands`;
 
 const getBrand = async (id: string): Promise<Brand> => {
   const res = await fetch(`${URL}/${id}`);
