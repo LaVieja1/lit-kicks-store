020b58570f0710eca54f13792b5c1e1ac0cf8e76
santicano <canosantiago404@gmail.com>
2024-02-25 14:40:42 -0300

    rediseñado shopping cart

--- app/(routes)/cart/_components/summary.tsx
+++ app/(routes)/cart/_components/summary.tsx
@@ -42,18 +42,18 @@ const Summary = () => {
   };
 
   return (
-    <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
-      <h2 className="text-lg font-medium text-gray-900">Order Summary</h2>
+    <div className="mt-16 rounded-lg bg-black px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
+      <h2 className="text-lg font-medium text-green">Order Summary</h2>
       <div className="mt-6 space-y-4">
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
-          <div className="text-base font-medium text-gray-900">Order total</div>
+          <div className="text-base font-medium text-white">Order total</div>
           <Currency value={totalPrice} />
         </div>
       </div>
       <Button
         disabled={items.length === 0}
         onClick={onCheckout}
-        className="w-full mt-6"
+        className="w-full mt-6 bg-green text-black uppercase font-bold"
       >
         Checkout
       </Button>
